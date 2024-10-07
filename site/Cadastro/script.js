34ce5a4b0121605d400f0c4ec9d14b20366a9d59
phmpereira <pedro.pereira@sptech.school>
2024-10-07 16:30:19 -0300

    add: adicionado indicador visual de força da senha

--- site/Cadastro/script.js
+++ site/Cadastro/script.js
@@ -1,3 +1,41 @@
+function verificar_requisitos(){
+  var senha = senha_input.value
+  var tem_min = false
+  var tem_max = false
+
+  if(senha.length >= 8){
+    req_char.style.color = '#228B22'
+    req_char.innerHTML = `<i class='bx bxs-like'></i>8 caracteres`
+  }else{
+    req_char.style.color = '#CC0000'
+    req_char.innerHTML = `<i class='bx bxs-dislike'></i>8 caracteres`
+  }
+
+  if(senha != senha.toUpperCase()){
+    req_min.style.color = '#228B22'
+    req_min.innerHTML = `<i class='bx bxs-like'></i>Letras minúsculas`
+  }else{
+    req_min.style.color = '#CC0000'
+    req_min.innerHTML = `<i class='bx bxs-dislike'></i>Letras minúsculas`
+  }
+
+  if(senha != senha.toLowerCase()){
+    req_mai.style.color = '#228B22'
+    req_mai.innerHTML = `<i class='bx bxs-like'></i>Letras maiúsculas`
+  }else{
+    req_mai.style.color = '#CC0000'
+    req_mai.innerHTML = `<i class='bx bxs-dislike'></i>Letras maiúsculas`
+  }
+
+  if(senha.includes('@') || senha.includes('#') || senha.includes('$') || senha.includes('%') || senha.includes('&') || senha.includes('_')){
+    req_esp.style.color = '#228B22'
+    req_esp.innerHTML = `<i class='bx bxs-like'></i>Caractere especial`
+  }else{
+    req_esp.style.color = '#CC0000'
+    req_esp.innerHTML = `<i class='bx bxs-dislike'></i>Caractere especial`
+  }
+}
+
 function verificar_senha(){
   var senha = senha_input.value
   var conf_senha = confirmacao_senha_input.value
